f38b903ca735105e82bad73dd37dfc086f8cec6a
Dhanasekar <dhanasekarr31@gmail.com>
2026-06-11 16:56:36 +0530

    Updated project

--- ems-updated/backend/index.js
+++ ems-updated/backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: "https://employee-management-system-delta-eight-95.vercel.app/" }));
+app.use(cors({ origin: "https://heartfelt-figolla-56b965.netlify.app/" }));
 app.use(express.json());
 
 // Routes
